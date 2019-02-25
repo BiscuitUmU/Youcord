e1ec76ac9b6a64b73aff8846698ce7af3cccb207
Arias800 <azertypoil630@gmail.com>
2019-02-25 16:57:04 +0100

    Fix Youtube Original video

--- src/utils/youtubeHelper.js
+++ src/utils/youtubeHelper.js
@@ -19,14 +19,20 @@ exports.getVideoInfo = async function (videoId) {
         }
     })
         .then(function (response) {
-            return response.json();
+        	result = response.json()
+        	//console.log(response)
+            return result;
         })
         .then(function (jsd) {
             ytr.title = jsd[2].player.args.title
             ytr.uploader = jsd[2].player.args.author
             ytr.likes = jsd[3].response.contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.sentimentBar.sentimentBarRenderer.tooltip.split(' / ')[0]
             ytr.dislikes = jsd[3].response.contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.sentimentBar.sentimentBarRenderer.tooltip.split(' / ')[1]
-            ytr.views = jsd[3].response.contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.viewCount.videoViewCountRenderer.viewCount.simpleText.split(' ')[0]
+            try{
+                ytr.views = jsd[3].response.contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.viewCount.videoViewCountRenderer.viewCount.simpleText.split(' ')[0]
+            }catch (e) {//If it's an Youtube Original there haven't view count
+                ytr.views = ''
+            }
             try {
                 ytr.category = jsd[3].response.contents.twoColumnWatchNextResults.results.results.contents[1].videoSecondaryInfoRenderer.metadataRowContainer.metadataRowContainerRenderer.rows[1].metadataRowRenderer.contents[0].runs[0].text
             } catch (e) {
